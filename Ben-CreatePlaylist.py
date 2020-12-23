0c27e1d9fa0740fbf6e402deb560302ce5071a3f
Benjamin Schraeder <benschra@rams.colostate.edu>
2020-12-23 13:11:14 -0700

    CreatePlaylist scaffold

---
+++ Ben-CreatePlaylist.py
@@ -0,0 +1,18 @@
+class CreatePlaylist:
+    def __init__(self):
+        pass
+
+    def get_youtube_client(self):
+        pass
+
+    def get_liked_videos(self):
+        pass
+
+    def create_playlist(self):
+        pass
+
+    def get_spotify_url(self):
+        pass
+
+    def add_song_to_playlist(self):
+        pass
